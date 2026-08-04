a7065b91056e01d8661583b69a48ac2e2eab4d04
BRBX <egorka2005sautkin@gmail.com>
2026-08-04 14:52:16 +0300

    Testing

--- brbx-mvi/api/build.gradle.kts
+++ brbx-mvi/api/build.gradle.kts
@@ -2,8 +2,17 @@ plugins {
     alias(libs.plugins.brbxmvi.kotlin.library)
 }
 
+tasks.test {
+    useJUnit()
+}
+
 dependencies {
 
     // ViewModel
-    implementation(libs.androidx.lifecycle.viewmodel)
+    api(libs.androidx.lifecycle.viewmodel)
+
+    // Testing
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.turbine)
+    testImplementation(libs.junit)
 }
